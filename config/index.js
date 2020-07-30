1c13b2b19a2f319ab25cc00daa581dcff48eb3ac
cenjianyuan <1714130904@qq.com>
2020-07-30 10:01:59 +0800

    s

--- config/index.js
+++ config/index.js
@@ -9,7 +9,7 @@ module.exports = {
 
     // Paths
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/',
+    assetsPublicPath: '/cenjianyaun/',
     proxyTable: {},
 
     // Various Dev Server settings
